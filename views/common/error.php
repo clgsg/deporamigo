740cf2eab23c64ff9ce60c19515adb9a6bac18be
clgsg <clg.listas@gmail.com>
2024-10-22 16:58:54 +0200

    Error "user-friendly" en pantalla, detalles en log

---
+++ views/common/error.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DeporAmigo-Error</title>
+</head>
+<body>
+    <h2>Algo ha fallado...</h2>
+</body>
+</html>
