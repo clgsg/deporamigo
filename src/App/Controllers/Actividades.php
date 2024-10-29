63266cf90e9019a7ff4d3df445300d3603892d24
clgsg <clg.listas@gmail.com>
2024-10-29 11:21:35 +0100

    Fix typo getActivityData

--- src/App/Controllers/Actividades.php
+++ src/App/Controllers/Actividades.php
@@ -3,14 +3,14 @@
 namespace App\Controllers;
 
 # 'use' indicates the correct namespace for Actividad, no need to prepend the whole route
-use \App\Models\Actividad;
+use App\Models\Actividad;
 use Common\Viewer;
 
 
 class Actividades {
     public function view(){
         $model = new Actividad;
-        $actividades = $model->geActivityData();
+        $actividades = $model->getActivityData();
 
         $viewer = new Viewer;
         echo $viewer -> render("common/header.php", ["title" => "DeporAmigo - Actividades"]);
@@ -21,12 +21,24 @@ class Actividades {
     }
     public function show(){
         $model = new Actividad;
-        $actividades = $model->geActivityData();
+        $actividades = $model->getActivityData();
         
         $viewer = new Viewer;
         
         echo $viewer -> render("common/header.php", ["title" => "DeporAmigo - Actividades"]);
         echo $viewer -> render("Actividades/show.php", ["actividades" => $actividades]);
+        echo $viewer -> render("common/footer.php");
+    }
+
+
+    public function add($apodo, $deporte, $fecha, $nombre_instalacion, $min_jugadores=null, $max_jugadores=null){
+        $model = new Actividad;
+        $actividades = $model->addActivity($apodo, $deporte, $fecha, $nombre_instalacion, $min_jugadores=null, $max_jugadores=null);
+        
+        $viewer = new Viewer;
+        
+        echo $viewer -> render("common/header.php", ["title" => "DeporAmigo - Actividades"]);
+        echo $viewer -> render("Actividades/add.php", ["actividades" => $actividades]);
         echo $viewer -> render("common/footer.php");
 
 
